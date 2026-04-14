1c0706f1f4e324727965afb8f0c40c43502b7170
elhadi-zertal <med.zertal@gmail.com>
2026-04-14 15:51:56 +0100

    added a tool to response

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from agno.agent import Agent
 from agno.models.groq import Groq
 from dotenv import load_dotenv
+from agno.tools.yfinance import YFinanceTools
 
 load_dotenv()
 
@@ -9,7 +10,8 @@ agent = Agent(
     model=Groq(
         id="llama-3.1-8b-instant",
         api_key=os.environ['GROQ_API_KEY']
-    )
+    ),
+    tools=[YFinanceTools()],
 )
 
 agent.print_response("What is the stock price of Apple?", stream=True)
