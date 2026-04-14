97c103c5a1822883a5f78d86824ce90f92966e67
elhadi-zertal <med.zertal@gmail.com>
2026-04-14 21:52:48 +0100

    added storage and memory

--- main.py
+++ main.py
@@ -2,30 +2,42 @@ import os
 from agno.agent import Agent
 from agno.models.groq import Groq
 from dotenv import load_dotenv
-from agno.tools.yfinance import YFinanceTools
-from agno.tools.reasoning import ReasoningTools
+from agno.db.sqlite import SqliteDb
+#from agno.tools.yfinance import YFinanceTools
+#from agno.tools.reasoning import ReasoningTools
+
 
 load_dotenv()
 
+db = SqliteDb(db_file="tmp/agents.db")
+
 agent = Agent(
+    session_id=1,
+    user_id="admin",
     model=Groq(
         id="llama-3.1-8b-instant",
         api_key=os.environ['GROQ_API_KEY']
     ),
+
+    db=db,
+    add_history_to_context=True,
+    update_memory_on_run=True,
     
-    instructions=[
-        "Include sources in your response",
-    ],
+    #instructions=[
+    #    "Include sources in your response",
+    #],
 
-    tools=[
-        ReasoningTools(),
-        YFinanceTools()
-    ],
+    #tools=[
+    #    ReasoningTools(),
+    #    YFinanceTools()
+    #],
 )
 
-agent.print_response(
-    "Write a report on AMD?",
-    show_full_reasoning=True,
-    stream_intermediate_steps=True,
-)
+#agent.print_response(
+#    "Write a report on AMD?",
+    #show_full_reasoning=True,
+    #stream_intermediate_steps=True,
+#)
 
+agent.print_response("My name is el hadi")
+agent.print_response("What is my name?")
